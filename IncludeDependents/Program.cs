fa8ca229b25f63adff27fea747b6c7286ed20385
Leonard Thieu <leonard.thieu@gmail.com>
2019-08-09 10:02:52 -0400

    Add project files.

---
+++ IncludeDependents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IncludeDependents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
